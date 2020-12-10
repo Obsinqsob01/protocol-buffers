e34fac2733d0dfc5dc161ec94b2d234b78d81c57
Luis Contreras Guerra <luis@mailvibes.io>
2020-12-10 00:13:25 -0600

    Create feature to list all tasks

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bytes"
+	"encoding/gob"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"strings"
 
@@ -52,6 +55,11 @@ func add(text string) error {
 	if err != nil {
 		return fmt.Errorf("could not open %s: %v", dbPath, err)
 	}
+
+	if err := gob.NewEncoder(f).Encode(int64(len(b))); err != nil {
+		return fmt.Errorf("could not encode length of message: %v", err)
+	}
+
 	_, err = f.Write(b)
 	if err != nil {
 		return fmt.Errorf("could not write task to file: %v", err)
@@ -65,5 +73,38 @@ func add(text string) error {
 }
 
 func list() error {
+	b, err := ioutil.ReadFile(dbPath)
+	if err != nil {
+		return fmt.Errorf("could not read %s: %v", dbPath, err)
+	}
+
+	for {
+		if len(b) == 0 {
+			return nil
+		} else if len(b) < 4 {
+			return fmt.Errorf("remaining odd %d bytes, what to do?", len(b))
+		}
+
+		var length int64
+		if err := gob.NewDecoder(bytes.NewReader(b[:4])).Decode(&length); err != nil {
+			return fmt.Errorf("could not decode message length: %v", err)
+		}
+		b = b[4:]
+
+		var task todo.Task
+		if err := proto.Unmarshal(b[:length], &task); err != nil {
+			return fmt.Errorf("could not read task: %v", err)
+		}
+		b = b[length:]
+
+		if task.Done {
+			fmt.Printf("+1")
+		} else {
+			fmt.Println("-1")
+		}
+
+		fmt.Printf(" %s\n", task.Text)
+	}
+
 	return nil
 }
